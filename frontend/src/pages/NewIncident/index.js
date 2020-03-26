86efbacd9a15217570447e4ba39d1cf54e3d4e6a
Michael Lean Pedrasoli <pedrasolimichael@gmail.com>
2020-03-26 11:57:05 -0300

    Front-end - fix url

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -19,7 +19,7 @@ export default function NewIncident() {
   async function handleSubmit(e) {
     e.preventDefault();
     try {
-      await api.post('incidentsftp', {
+      await api.post('incidents', {
         title,
         description,
         value
